9d75698e63b671084deadd8aa5a658f396d4d74d
jjaw89 <81355460+jjaw89@users.noreply.github.com>
2024-03-18 10:06:28 -0700

    I think I'm ready to extract all the data.

--- slp_package/slp_enumerations.py
+++ slp_package/slp_enumerations.py
@@ -1,8 +1,16 @@
 from enum import Enum
 
+# Enumeration of the inputs in the np arrays containing the input data
 class InputDataKey(Enum):
-    JOYSTICK_X = 0
-    JOYSTICK_Y = 1
+    JSTICK_X = 0
+    JSTICK_Y = 1
+    CSTICK_X = 2
+    CSTICK_Y = 3
+    TRIGGER_LOGICAL = 4
+    Z = 5
+    A = 6
+    B = 7
+    X_or_Y = 8
     
 class Color(Enum):
     RED = 1
