433cbf1b712dfd064454019fe497ba1164855e5c
sinha-G <gsinha@uwaterloo.ca>
2024-03-23 12:24:14 -0700

    classify mango or null

--- slp_package/slp_functions.py
+++ slp_package/slp_functions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def one_hot_encode_flags(flag_enum, bitmask):
     """
     One-hot encode the set flags from the given bitmask using the specified IntFlag enumeration.
@@ -15,3 +17,35 @@ def one_hot_encode_flags(flag_enum, bitmask):
             one_hot_encoded[i] = 1
 
     return one_hot_encoded
+
+def create_merged_game_data_df(df_list, no_teams_2_player = True):
+    '''
+    Returns a merged dataframe comprised of the mang0, ranked, and/or public datasets.
+
+    :df_list: List of Strings. Each String is one of:
+                 * mango
+                 * ranked
+                 * public
+    :no_teams_2_player: Boolean. Returns all data if False. Returns only 2 player games if True
+    '''
+    df = pd.DataFrame()
+
+    # Either get all data or 2 player data depending on the value passed to function
+    filter = 'no_teams_2_players' if no_teams_2_player else 'all_game_data'
+    
+    if 'mango' in df_list:
+        df = pd.concat([df,
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\mango_" + filter + "_df.parquet"),
+                        ])
+    if 'ranked' in df_list:
+        df = pd.concat([df,
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\ranked_" + filter + "_df_1.parquet"),
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\ranked_" + filter + "_df_2.parquet"),
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\ranked_" + filter + "_df_3.parquet"),
+                        ])
+    if 'public' in df_list:
+        df = pd.concat([df, 
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\public_" + filter + "_df_1.parquet"),
+                        pd.read_parquet("C:\\Users\\jaspa\\Grant ML\\slp\\data\\public_" + filter + "_df_2.parquet"),
+                        ])
+    return df
